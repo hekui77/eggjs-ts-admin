a82e03e5abe18c65a7aafe5908f5b1e648575257
hekui <hekui@tydic.com>
2022-11-29 19:48:30 +0800

    部署测试

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -2,11 +2,13 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportHome from '../../../app/controller/home';
 import ExportTest from '../../../app/controller/test';
 import ExportUser from '../../../app/controller/user';
 
 declare module 'egg' {
   interface IController {
+    home: ExportHome;
     test: ExportTest;
     user: ExportUser;
   }
