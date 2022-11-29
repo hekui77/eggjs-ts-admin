ca878a21ef0afbbc0668f70d3491e002718cf737
hekui <hekui@tydic.com>
2022-11-29 18:45:07 +0800

    修改测试代码

--- test/app/controller/test.test.ts
+++ test/app/controller/test.test.ts
@@ -2,7 +2,7 @@ import assert from 'assert';
 import { app } from 'egg-mock/bootstrap';
 
 describe('test/app/controller/test.test.ts', () => {
-  it('should GET /', async () => {
+  it('should GET /api/test', async () => {
     const result = await app.httpRequest().get('/').expect(200);
     assert(result.text === 'hi, egg');
   });
